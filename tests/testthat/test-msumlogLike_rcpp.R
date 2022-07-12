673e30150c904c19327686e803c30d2523cf64af
maltelueken <malte_lueken@arcor.de>
2022-07-12 11:10:54 +0200

    Add tolerance to tests to avoid floating point imprecision

--- tests/testthat/test-msumlogLike_rcpp.R
+++ tests/testthat/test-msumlogLike_rcpp.R
@@ -33,17 +33,17 @@ p = attr(get(test_obj_name), 'pMat')
 testthat::test_that("State likelihood works", {
   ref = 0.5
   ll_state = m4ma::like_state_rcpp(trace_i[[1]], p, n = 3, nests, alpha, m4ma::get_cell_nest())
-  testthat::expect_equal(ll_state, ref)
+  testthat::expect_equal(ll_state, ref, tolerance = 1e-5)
 })
 
 testthat::test_that("State likelihood loop works", {
   ref = c(-Inf, -487.1970260, -0.6931472, -Inf, -14.8647133, -Inf, -Inf, -309.3628411, -Inf)
   ll_states = m4ma::like_states_rcpp(trace_i, p, nests, alpha, m4ma::get_cell_nest())
-  testthat::expect_equal(log(unlist(ll_states)), ref)
+  testthat::expect_equal(log(unlist(ll_states)), ref, tolerance = 1e-5)
 })
 
 testthat::test_that("Trace likelihood sum works", {
   ref = 176.738817
   ll_sum = m4ma::msumlogLike_rcpp(trace_i, p, minLike = 1e-10, mult = -1)
-  testthat::expect_equal(ll_sum, ref)
+  testthat::expect_equal(ll_sum, ref, tolerance = 1e-5)
 })
