30e4c204c7e824196bea9f918d90ba43ae56bb05
maltelueken <malte_lueken@arcor.de>
2022-10-12 11:52:42 +0200

    Fix wrapper for line_line_intersection()

--- R/see_wrappers.R
+++ R/see_wrappers.R
@@ -3,7 +3,14 @@
 #' @rdname line_line_intersection_rcpp
 #' @param ... Arguments passed to the function implementation.
 #' @param use Whether the R or C++ implementation is called.
-line.line.intersection = wrapper('line_line_intersection')
+line.line.intersection = function(P1, P2, P3, P4, interior.only, use = 'cpp') {
+  if (use == 'r' || (exists('predped_env') && predped_env$use == 'r')) {
+    return(m4ma::line_line_intersection_r(P1, P2, P3, P4, interior.only))
+  } else {
+    return(m4ma::line_line_intersection_rcpp(P1, P2, P3, P4,
+                                             interior_only = interior.only))
+  }
+}
 
 
 #' @rdname seesGoal_rcpp
